63350ac3e457c6a1805680db5b5513c80e14eba4
Kaloyan Vulchev <kvalchevv1@gmail.com>
2024-06-11 18:38:40 +0300

    vercel

--- server/src/app.ts
+++ server/src/app.ts
@@ -26,7 +26,7 @@ app.use(
   cors({
     credentials: true,
     origin: [client],
-    methods: ["GET", "POST", "PUT", "DELETE", "HEAD", "PATCH"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   })
 );
 
